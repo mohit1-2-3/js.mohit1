929212195a8aa6b578bdf54bf11224adb17c6b3a
mohit1-2-3 <mv2944286@gmail.com>
2024-09-27 06:55:00 +0000

    variable js

---
+++ basic/variable.js
@@ -0,0 +1,12 @@
+const accountId=12232
+let accountEmail="mohitvarma2022@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+//accountId=1223
+accountEmail="mohitvarma202@gmail.com"
+accountPassword="1234"
+accountCity="daily"
+let accountState;
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
