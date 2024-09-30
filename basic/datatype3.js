63e3ba229bd666c8c751ffe72d81daa97aa8e7bb
mohit1-2-3 <mv2944286@gmail.com>
2024-09-30 09:42:47 +0000

    datatype3

---
+++ basic/datatype3.js
@@ -0,0 +1,42 @@
+// primitive
+
+// 7 types: String, Number, Boolean, null, Undefind, Symbol BIgInt
+// console.log("mohit");
+
+// Reference (NOn primitive)
+
+//Array, Objects, Function
+ const score=100
+ const score1=100.3
+ const isLoggedIn =false
+ const outsideTemp=null
+
+ const id= Symbol('123')
+ const anotherId=Symbol('123')
+console.log(id=== anotherId);
+
+const bigNumber=42435435379327n
+
+
+const heros=["shaktiman", "naagraj", " doga"];
+    let myObj={
+
+        name: "mohit",
+        age: 22,
+      
+}
+
+  const myFunction=function(){
+
+    console.log("hello world");
+    
+  }
+
+  console.log(typeof bigNumber);
+  console.log(typeof myFunction);
+  console.log(typeof heros);
+  console.log(typeof anotherId);
+
+  //http://262.ecma-international.org/5.1/#sec-11.4.3
+  
+  
